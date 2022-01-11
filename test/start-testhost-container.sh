42bc09a7f1fc18a806fb0aa10e71e0fb9f9389cf
Jonathan Swinney <jswinney@amazon.com>
2022-01-11 02:09:30 +0000

    update upstream repository url

--- test/start-testhost-container.sh
+++ test/start-testhost-container.sh
@@ -8,6 +8,6 @@ docker run -it --rm \
     -v $(pwd)/test:/io \
     -v $(pwd):/repo \
     --env WORK_PATH=$(realpath test) \
-    --env GITHUB_REPOSITORY="AWSjswinney/arm64-python-wheel-tester" \
+    --env GITHUB_REPOSITORY="geoffreyblake/arm64-python-wheel-tester" \
     --env GITHUB_API_URL="https://api.github.com" \
     testhost
